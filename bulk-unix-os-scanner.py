f34f6f1e02c0aefe18d8b1f1f1aac87ce243898c
Yohan Wongso <40362171+yohanwongso@users.noreply.github.com>
2023-07-23 02:49:29 +0700

    Update bulk-unix-os-scanner.py

--- bulk-unix-os-scanner.py
+++ bulk-unix-os-scanner.py
@@ -24,25 +24,24 @@ def get_os_info (ssh_client):
 
 with open('config.yml', 'r') as yaml_config_file:
     config = safe_load(yaml_config_file)
-    yaml_config_file.close()
 
-    with open(config["ssh"]["list_ip_file"], "r") as list_ip_file:
-        with open(config["output"]["csv"], 'w', newline='', encoding='utf-8') as output_csv_file:
-            output_csv_writer = writer(output_csv_file)
-            csv_header = ["IP", "OS Info"]
-            output_csv_writer.writerow(csv_header)
+with open(config["ssh"]["list_ip_file"], "r") as list_ip_file:
+    with open(config["output"]["csv"], 'w', newline='', encoding='utf-8') as output_csv_file:
+        output_csv_writer = writer(output_csv_file)
+        csv_header = ["IP", "OS Info"]
+        output_csv_writer.writerow(csv_header)
 
-            for ip_with_enter in list_ip_file:
-                ip = ip_with_enter.replace("\n", "")
-                print(ip)
-                ssh_client = create_SSH_client(ip, config["ssh"]["port"], config["ssh"]["username"], config["ssh"]["password"])
+        for ip_with_enter in list_ip_file:
+            ip = ip_with_enter.replace("\n", "")
+            print(ip)
+            ssh_client = create_SSH_client(ip, config["ssh"]["port"], config["ssh"]["username"], config["ssh"]["password"])
 
-                if ssh_client != False:
-                    os_info = get_os_info(ssh_client)
-                    csv_data = [ip, os_info]
-                    output_csv_writer.writerow(csv_data)
-                else:
-                    csv_data = [ip, ""]
-                    output_csv_writer.writerow(csv_data)
+            if ssh_client != False:
+                os_info = get_os_info(ssh_client)
+                csv_data = [ip, os_info]
+                output_csv_writer.writerow(csv_data)
+            else:
+                csv_data = [ip, ""]
+                output_csv_writer.writerow(csv_data)
 
-                ssh_client.close()
+            ssh_client.close()
